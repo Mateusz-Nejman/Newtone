adfc73a122e31e4a759afc97053deab00a97a314
Mateusz Nejman <mateusz_nejman@hotmail.com>
2020-10-17 12:52:00 +0200

    Bugfix, new screenshots

--- Newtone.Mobile/Media/MediaPlayerHelper.cs
+++ Newtone.Mobile/Media/MediaPlayerHelper.cs
@@ -8,27 +8,37 @@ namespace Newtone.Mobile.Media
         #region Public Methods
         public static void Play()
         {
-            MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.Play();
+            if (CanBeUsed())
+                MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.Play();
         }
 
         public static void Pause()
         {
-            MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.Pause();
+            if (CanBeUsed())
+                MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.Pause();
         }
 
         public static void Stop()
         {
-            MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.Stop();
+            if (CanBeUsed())
+                MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.Stop();
         }
 
         public static void Next()
         {
-            MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.SkipToNext();
+            if (CanBeUsed())
+                MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.SkipToNext();
         }
 
         public static void Prev()
         {
-            MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.SkipToPrevious();
+            if (CanBeUsed())
+                MediaControllerCompat.GetMediaController(MainActivity.Instance ?? MediaPlayerService.Instance.GetActivity())?.GetTransportControls()?.SkipToPrevious();
+        }
+
+        private static bool CanBeUsed()
+        {
+            return MainActivity.Instance != null || MediaPlayerService.Instance?.GetActivity() != null;
         }
         #endregion
     }
